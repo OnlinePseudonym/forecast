363be61a36599885010f80bdec0db5f0534b6d74
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-02-06 11:52:09 -0700

    add google maps to chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const { lat, lon } = cityData.city.coord;
         const temps = cityData.list
             .map(weather => weather.main.temp)
             .map(temp => 9 / 5 * (temp - 273) + 32)
@@ -16,7 +18,7 @@ class WeatherList extends Component {
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="orange" units="&deg;F" /></td>
                 <td><Chart data={pressures} color="green" units="hPa" /></td>
                 <td><Chart data={humidities} color="black" units="%" /></td>
